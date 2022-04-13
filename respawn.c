69207c493561abaa5ff99a4d6b100259b33eef3b
binotation <68807919+binotation@users.noreply.github.com>
2022-04-13 21:16:34 +1000

    Renamed filepath to binpath

--- respawn.c
+++ respawn.c
@@ -3,7 +3,7 @@
 #include <signal.h>
 #include <string.h>
 
-static char *filepath;
+static char *binpath;
 static char *stdout_redirect;
 static char *stderr_redirect;
 static char **bin_args;
@@ -12,13 +12,13 @@ void child_exit_handler(int sig) {
     if (!fork()) {
         freopen(stdout_redirect, "w+", stdout);
         freopen(stderr_redirect, "w+", stderr);
-        execvp(filepath, bin_args);
+        execvp(binpath, bin_args);
     }
 }
 
 int main(int argc, char **argv) {
     if (argc < 4) {
-        fputs("Usage: respawn filepath stdout_redirect stderr_redirect bin_args...\n", stderr);
+        fputs("Usage: respawn binpath stdout_redirect stderr_redirect bin_args...\n", stderr);
         return 1;
     }
 
@@ -27,7 +27,7 @@ int main(int argc, char **argv) {
     sa.sa_handler = child_exit_handler;
     sigaction(SIGCHLD, &sa, NULL);
 
-    filepath = argv[1];
+    binpath = argv[1];
     stdout_redirect = argv[2];
     stderr_redirect = argv[3];
     bin_args = &argv[3];
