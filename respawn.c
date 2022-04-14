39b7137fb8f87c15ac76b1314c50da379e098d0c
binotation <68807919+binotation@users.noreply.github.com>
2022-04-14 11:39:08 +1000

    Change implementation to use only wait()

--- respawn.c
+++ respawn.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <signal.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
@@ -13,11 +11,6 @@
     execvp(binpath, bin_args);\
 }
 
-static char *binpath;
-static char *stdout_redirect;
-static char *stderr_redirect;
-static char **bin_args;
-
 void cpy_slice_into(char **args, char **argv_slice) {
     int i = -1;
     while (argv_slice[++i] != 0) {
@@ -26,35 +19,31 @@ void cpy_slice_into(char **args, char **argv_slice) {
     args[i] = 0;
 }
 
-void child_exit_handler(int sig) {
-    if (!fork()) {
-        CHLD_INIT_ROUTINE()
-    }
-}
-
 int main(int argc, char **argv) {
     if (argc < 4) {
         fputs("Usage: respawn binpath stdout_redirect stderr_redirect bin_args...\n", stderr);
         return 1;
     }
 
-    // Initialize SIGCHLD handler
-    struct sigaction sa;
-    memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = child_exit_handler;
-    sigaction(SIGCHLD, &sa, NULL);
+    char *binpath = argv[1];
+    char *stdout_redirect = argv[2];
+    char *stderr_redirect = argv[3];
 
-    binpath = argv[1];
-    stdout_redirect = argv[2];
-    stderr_redirect = argv[3];
-
-    bin_args = (char**)malloc(sizeof(char*) * (argc - 2));
+    char **bin_args = malloc(sizeof(char*) * (argc - 2));
     cpy_slice_into(bin_args, &argv[3]);
     bin_args[0] = binpath;
 
     if (!fork()) {
+        // Spawn initial child process
         CHLD_INIT_ROUTINE()
     } else {
-        while (1) { wait(0); }
+        while (1) {
+            // Wait for child process termination
+            if (wait(0) > 0) {
+                if (!fork()) {
+                    CHLD_INIT_ROUTINE()
+                }
+            };
+        }
     }
 }
